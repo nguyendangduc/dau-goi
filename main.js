0021185388eda923375fc2e54ebe2cf971747ab7
Duc Dang nguyen <ducdn@hitexglobal.com>
2021-09-19 06:30:35 +0700

    2

--- main.js
+++ main.js
@@ -9,65 +9,89 @@ function scrollFunction() {
   }
 }
 $(function () {
-  $("ul.u-nav-1 li:nth-child(1) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(1)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-f23e").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-1 li:nth-child(2) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(2)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-f354").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-1 li:nth-child(3) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(3)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-d77a").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-1 li:nth-child(4) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(4)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-2062").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-1 li:nth-child(5) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(5)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-c064").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-1 li:nth-child(6) a").click(function () {
+  $("ul.u-nav-1 li:nth-child(6)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-1 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-e83e").offset().top - 78 }, 500);
 
   });
   
   // mobile
-  $("ul.u-nav-2 li:nth-child(1) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(1)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-f23e").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-2 li:nth-child(2) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(2)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-f354").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-2 li:nth-child(3) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(3)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-d77a").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-2 li:nth-child(4) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(4)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-2062").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-2 li:nth-child(5) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(5)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-c064").offset().top - 78 }, 500);
 
   });
-  $("ul.u-nav-2 li:nth-child(6) a").click(function () {
+  $("ul.u-nav-2 li:nth-child(6)").click(function () {
     event.preventDefault();
+    $("ul.u-nav-2 li").removeClass("active")
+    $(this).addClass("active")
     $('html').animate({ scrollTop: $("#sec-e83e").offset().top - 78 }, 500);
 
   });
