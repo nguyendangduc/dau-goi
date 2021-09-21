6b17e26ba1591fa96c0fa41d6401a656d237d920
Duc Dang nguyen <ducdn@hitexglobal.com>
2021-09-21 15:27:13 +0700

    2

--- main.js
+++ main.js
@@ -14,30 +14,27 @@ function scrollFunction() {
 }
 function updateNavbar() {
   let listSection = document.querySelectorAll("body section")
-  let listSectionMb = document.querySelectorAll("body section")
   const listItem = document.querySelectorAll("#sec-1d34 nav ul.u-nav-1 li")
   const listItemMb = document.querySelectorAll("#sec-1d34 nav ul.u-nav-2 li")
   console.log(listItem)
   console.log(listSection)
 
   for(let i = 0; i < listSection.length; i++) {
-   if(document.documentElement.scrollTop >= listSection[i].offsetTop - 78 ) {
+   if(document.documentElement.scrollTop >= listSection[i].offsetTop -79 ) {
       listItem.forEach(item => {
         item.classList.remove("active")
       })
+      listItemMb.forEach(item => {
+        item.classList.remove("active")
+        
+      })
       listItem[i].classList.add("active")
+      listItemMb[i].classList.add("active")
+
     }
       
   }
-  for(let i = 0; i < listSectionMb.length; i++) {
-    if(document.documentElement.scrollTop >= listSectionMb[i].offsetTop - 78 ) {
-       listItemMb.forEach(item => {
-         item.classList.remove("active")
-       })
-       listItemMb[i].classList.add("active")
-     }
-       
-   }
+
   if(document.documentElement.scrollTop >= document.querySelector("footer").offsetTop -500) {
     listItem.forEach(item => {
       item.classList.remove("active")
@@ -54,50 +51,50 @@ function updateNavbar() {
 $(function () {
   $("ul.u-nav-1 li:nth-child(1)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: document.querySelector(".u-section-1").offsetTop - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(2)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-2").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(3)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-3").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(4)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-4").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(5)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-5").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(6)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-6").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-1 li:nth-child(7)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-1 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-1 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $("footer").offset().top - 78 }, 500);
 
   });
@@ -105,50 +102,50 @@ $(function () {
   // mobile
   $("ul.u-nav-2 li:nth-child(1)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-1").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(2)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-2").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(3)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-3").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(4)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-4").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(5)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-5").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(6)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $(".u-section-6").offset().top - 78 }, 500);
 
   });
   $("ul.u-nav-2 li:nth-child(7)").click(function () {
     event.preventDefault();
-    $("ul.u-nav-2 li").removeClass("active")
-    $(this).addClass("active")
+    // $("ul.u-nav-2 li").removeClass("active")
+    // $(this).addClass("active")
     $('html').animate({ scrollTop: $("footer").offset().top - 78 }, 500);
 
   });
